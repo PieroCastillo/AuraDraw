68d1db3e8feb6248b6c5ecfaa8d07f68f49c7760
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-01-08 17:22:05 -0500

    Add files via upload

--- src/Aura.UI/Brushes/Blur.cs
+++ src/Aura.UI/Brushes/Blur.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using Avalonia.Collections;
 using Avalonia.Utilities;
 using System;
 using System.Collections.Generic;
@@ -20,14 +21,16 @@ namespace Aura.UI.Brushes
             var b_height = bitmap.Size.Height;
             var b_width = bitmap.Size.Width;
             var bmp = bitmap.Clone();
-
-            for ( var i = 0 ; i < b_width -1 ; )
+ 
+            var pixels = new AvaloniaList<Avalonia.PixelPoint>();
+            /* for ( int i = 0 ; i < b_width -1 ; )
             {
-               for ( var i = 0 ; i < b_height -1 ; )
+               for ( int g = 0 ; g < b_height -1 ;  )
                {
-               
+                
                }
-            }
+            } */
+    
             return null;
         }
     }
