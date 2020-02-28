93ae23f0d19bb756bef09536f80ddfd712b1a62e
PieroCastillo <pierocastillo2005@gmail.com>
2020-02-28 11:20:52 -0500

    uwu

--- src/AuraDraw.App/MainWindow.xaml.cs
+++ src/AuraDraw.App/MainWindow.xaml.cs
@@ -2,22 +2,39 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
+using System;
+using AuraDraw.App.windows;
 
 namespace AuraDraw.App
 {
     public class MainWindow : Window
     {
+        Button openbtn;
         public MainWindow()
         {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+          //  DispatcherTimer timer = new DispatcherTimer();
+          //  timer.Tick += new EventHandler(DispatcherTimer_Tick);
+          //  timer.Interval = new TimeSpan(0,0,1);
+          //  timer.Start();
+          openbtn = this.Find<Button>("openbtn");
+          openbtn.Click += DispatcherTimer_Tick;
+            
         }
-
         private void InitializeComponent()
         {
+            
             AvaloniaXamlLoader.Load(this);
         }
+
+        private void DispatcherTimer_Tick(object sender,EventArgs e){
+            var pwindow = new PrincipalWindow();
+            pwindow.Show();
+            this.Close();
+        }
+         
     }
 }
