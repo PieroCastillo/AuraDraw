7b48de1ead4023dcdcb90dabdc08e7cd45556415
PieroCastillo <pierocastillo2005@gmail.com>
2020-02-29 16:43:07 -0500

    #6

--- src/Aura.UI/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using Avalonia.Metadata;
-
-[assembly: XmlnsDefinition("https://github.com/PieroCastillo","Aura.UI.Controls")]
-[assembly: XmlnsDefinition("https://github.com/PieroCastillo","Aura.UI")]
-[assembly: XmlnsDefinition("https://github.com/PieroCastillo","Aura.UI.Brushes")]
