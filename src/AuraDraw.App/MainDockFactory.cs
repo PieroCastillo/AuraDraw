49d964bbd4e68737573d78ddf91a99acd891e32c
PieroCastillo <pierocastillo2005@gmail.com>
2020-02-29 19:43:29 -0500

    #4

--- src/AuraDraw.App/MainDockFactory.cs
+++ src/AuraDraw.App/MainDockFactory.cs
@@ -4,6 +4,18 @@ namespace AuraDraw.App
 {
     public class MainDockFactory : Factory
     {
-        
+        private object _context;
+
+        public MainDockFactory(object context)
+        {
+            _context = context;
+        }
+
+        public override IDock CreateLayout(){
+          
+          var root = CreateRootDock(); 
+          
+          return root; 
+        }
     }
 }
