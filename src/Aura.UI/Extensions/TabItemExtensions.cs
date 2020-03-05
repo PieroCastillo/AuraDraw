1ce8838aaae7e1f21be0c6e9586377fd67921064
PieroCastillo <pierocastillo2005@gmail.com>
2020-03-04 22:44:15 -0500

    developing UI/UX

---
+++ src/Aura.UI/Extensions/TabItemExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace Aura.UI.Extensions
+{
+    public static class TabItemExtensions
+    {
+        static void Close(this TabItem tab)
+        {
+
+        }
+    }
+}
