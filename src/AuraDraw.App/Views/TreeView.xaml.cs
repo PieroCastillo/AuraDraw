bc19aac91f7e1ea52d464f4fbeefa18b60ad59e8
PieroCastillo <pierocastillo2005@gmail.com>
2020-03-16 19:20:23 -0500

    Developing the AuraDraw's UI/UX

--- src/AuraDraw.App/Views/TreeView.xaml.cs
+++ src/AuraDraw.App/Views/TreeView.xaml.cs
@@ -1,19 +1,43 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 using Avalonia.Markup.Xaml;
 
 namespace AuraDraw.App.Views
 {
     public class TreeView : UserControl
     {
+        Avalonia.Controls.TreeViewItem treeView;
         public TreeView()
         {
             this.InitializeComponent();
+            treeView = this.Find<Avalonia.Controls.TreeViewItem>("tree");
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+        #region Properties
+        public string Title
+        {
+            get { return GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public readonly StyledProperty<string> TitleProperty =
+            AvaloniaProperty.Register<TreeView, string>(nameof(Title), "TitleDefault");
+
+        public IBrush TitleBrush
+        {
+            get { return GetValue(TitleBrushProperty); }
+            set { SetValue(TitleBrushProperty, value); }
+        }
+        public readonly StyledProperty<IBrush> TitleBrushProperty =
+            AvaloniaProperty.Register<TreeView, IBrush>(nameof(TitleBrush),Brushes.LightGreen);
+        #endregion
+        public void AddItem(TreeViewItem newItem , TreeViewItem owner)
+        {
+            owner.
         }
     }
 }
