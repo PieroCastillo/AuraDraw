d65192167e937259263af6ad371578b390fdfa4c
Piero Castillo <pierocastillo2005@gmail.com>
2020-05-30 23:12:05 -0500

    30/05/2020

--- src/AuraDraw.App/Functions/LayoutComponents/Layout.cs
+++ src/AuraDraw.App/Functions/LayoutComponents/Layout.cs
@@ -12,46 +12,8 @@ namespace AuraDraw.App.Functions.LayoutComponents
     {
         
     }
-    public class Layouts : ICollection<Layout>
+    public class Layouts : AvaloniaList<Layout>
     {
-        public Collection<Layout> Layouts_ = new Collection<Layout>();
-        public int Count => throw new NotImplementedException();
 
-        public bool IsReadOnly => throw new NotImplementedException();
-
-        public void Add(Layout item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(Layout item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(Layout[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<Layout> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(Layout item)
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
