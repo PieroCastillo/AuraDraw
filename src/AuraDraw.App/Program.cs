e2db9e9d57f3af756e7792b30bbaf06b95eb0ae2
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-08-04 00:31:56 -0500

    working

--- src/AuraDraw.App/Program.cs
+++ src/AuraDraw.App/Program.cs
@@ -11,6 +11,7 @@ namespace AuraDraw.App
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
 
+
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
